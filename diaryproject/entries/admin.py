4b5235d0c4ba0bfcefdc0d294ebe0275b6e47ff5
caeia <140228298+caesij@users.noreply.github.com>
2026-05-29 08:00:06 +0800

    Updated display of admin page
    - filtering by user and date creation
    - clickable user link at entry management
    - changed display of entry lists in admin page (entry title, user, and date created)

--- diaryproject/entries/admin.py
+++ diaryproject/entries/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import Entry
 
-admin.site.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ["title", "user_link", "created_at"]
+    list_filter = ["user", "created_at"]
+    search_fields = ["title", "user__username"]
+    ordering = ["-created_at"]
+    date_hierarchy = "created_at"
+
+    fieldsets = [
+        (Entry._meta.verbose_name_plural, {"fields": ["title", "content"]}),
+        ("Metadata", {"fields": ["user"]})
+    ]
+
+    def user_link(self, obj):
+        url = reverse("admin:auth_user_change", args=[obj.user.pk])
+        return format_html('<a href="{}">{}</a>', url, obj.user.username)
+    user_link.short_description = "User"
+
+admin.site.register(Entry, EntryAdmin)
